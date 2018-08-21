5b6d755ad6b2579a98eb477363321c718ea2e7ef
Simon Emanuel Schmid <simon@schmid.io>
2018-08-21 11:08:18 +0200

    Basic types & operations

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,36 @@
-import { add, toBigInt, BigInt } from "./";
+type BigInt = number;
+declare const BigInt: typeof Number;
+
+import {
+  add,
+  subtract,
+  toBigInt,
+  Token,
+  createQuantity,
+  Quantity,
+  Price
+} from "./index";
+
+const tokenA: Token = {
+  symbol: "TKNA",
+  address: "0x0",
+  decimals: 3
+};
+
+const tokenB: Token = {
+  symbol: "TKNB",
+  address: "0x1",
+  decimals: 4
+};
+
+const tokenC: Token = {
+  symbol: "TKNC",
+  address: "0x2",
+  decimals: 2
+};
+
+const quantityA1 = createQuantity(tokenA, toBigInt(2000));
+const quantityA2 = createQuantity(tokenA, toBigInt(3000));
 
 test("toBigInt", () => {
   expect(toBigInt(10)).toBe(BigInt(10));
@@ -9,6 +41,14 @@ test("toBigInt", () => {
   expect(toBigInt("0x123")).toBe(BigInt(291));
 });
 
-test("default", () => {
-  expect(add(1, 2)).toEqual(3);
+test("add", () => {
+  expect(add(quantityA1, quantityA2)).toEqual(
+    createQuantity(tokenA, toBigInt(5000))
+  );
+});
+
+test("subtract", () => {
+  expect(subtract(quantityA2, quantityA1)).toEqual(
+    createQuantity(tokenA, toBigInt(1000))
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /**
  * TODO:
  *
- * - [ ] toBigInt function
- * - [ ] Token Type: Symbol, Address, Decimals
- * - [ ] Quantity Type: Token, Quantity in token
- * - [ ] Price Type: BaseToken, QuoteToken, canceled down buy/sell
+ * - [x] toBigInt function
+ * - [x] Token Type: Symbol, Address, Decimals
+ * - [x] Quantity Type: Token, Quantity in token
+ * - [x] Price Type: BaseToken, QuoteToken, canceled down buy/sell
  * - [ ] valueIn (BaseToken * Price or QuoteToken / Price) // Check if meaningful
  * - [ ] add/subtract (check if same tokens)
  * - [ ] getPrice(quoteQuantity, baseQuantity): Price
@@ -15,9 +15,66 @@
 type BigInt = number;
 declare const BigInt: typeof Number;
 
-const toBigInt = (num: BigInt | Number | String) => BigInt(num);
-const add = (a: BigInt, b: BigInt): BigInt => a + b;
+export interface Token {
+  readonly symbol: string;
+  readonly address: string;
+  readonly decimals: number;
+}
 
-console.log(add(BigInt("1324"), BigInt("1324")));
+export interface Quantity extends Token {
+  readonly quantity: BigInt;
+}
 
-export { add, toBigInt };
+export interface Price {
+  readonly base: Quantity;
+  readonly quote: Quantity;
+}
+
+class RequireError extends Error {
+  data: any;
+
+  constructor(message: string, context: any) {
+    super(message);
+    this.data = context;
+    Object.setPrototypeOf(this, RequireError.prototype);
+  }
+}
+
+export const require = (
+  test: boolean,
+  message: string = "A requirement was not met",
+  context?: any
+): void => {
+  if (!test) throw new RequireError(message, context);
+};
+
+export const isSameToken = (a: Token, b: Token) =>
+  a.symbol === b.symbol && a.address === b.address && a.decimals === b.decimals;
+
+export const requireSameToken = (a: Token, b: Token): void =>
+  require(isSameToken(a, b), "Require same tokens", { a, b });
+
+export const createQuantity = (token: Token, quantity: BigInt): Quantity => ({
+  ...token,
+  quantity
+});
+
+export const toBigInt = (num: BigInt | Number | String) => BigInt(num);
+
+export const add = (a: Quantity, b: Quantity): Quantity => {
+  requireSameToken(a, b);
+
+  return {
+    ...a,
+    quantity: a.quantity + b.quantity
+  };
+};
+
+export const subtract = (a: Quantity, b: Quantity): Quantity => {
+  requireSameToken(a, b);
+
+  return {
+    ...a,
+    quantity: a.quantity - b.quantity
+  };
+};
