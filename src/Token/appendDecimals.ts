ef37faa26e5a7405806dd4dd09030fa7063e6d1b
Simon Emanuel Schmid <simon@schmid.io>
2018-10-03 15:30:03 +0200

    Fix array thing

--- src/Token/appendDecimals.ts
+++ src/Token/appendDecimals.ts
@@ -4,7 +4,7 @@ const appendDecimals = (token: TokenInterface, n: number): BigInt => {
   const string = n.toString();
   const int = parseInt(string).toString();
   const decimals = string.slice(int.length + 1);
-  const padding = Array(token.decimals - decimals.length)
+  const padding = Array.apply(null, Array(token.decimals - decimals.length))
     .map(() => 0)
     .join("");
 
