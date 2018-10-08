c6355ae51c375bcee022b3d621c3d90fd48ed451
Simon Emanuel Schmid <simon@schmid.io>
2018-10-08 18:51:12 +0200

    Note that toAtomic seems to be broken

--- src/Price/toAtomic.ts
+++ src/Price/toAtomic.ts
@@ -2,6 +2,7 @@ import divide from "../bigInteger/divide";
 
 import Price from "./Price";
 
+// TODO: Fix this, doesn't seem to work :(
 const toAtomic = (price: Price): string =>
   divide(price.quote.quantity, price.base.quantity).toString()
 
