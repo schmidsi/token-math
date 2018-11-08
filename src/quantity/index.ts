90c942baa6dc5110c42b98836622038b774b624c
Simon Emanuel Schmid <simon@schmid.io>
2018-11-08 15:01:57 +0100

    Export toFixed

--- src/quantity/index.ts
+++ src/quantity/index.ts
@@ -2,5 +2,6 @@ export { default as add } from "./add";
 export { default as createQuantity } from "./createQuantity";
 export { default as subtract } from "./subtract";
 export { default as isEqual } from "./isEqual";
+export { default as toFixed } from "./toFixed";
 export { default as greaterThan } from "./greaterThan";
 export { default as QuantityInterface } from "./QuantityInterface";
