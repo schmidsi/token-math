7925731b02ebe48fecc2c126f79e437292faedc1
Sebastian Siemssen <fubhy@fubhy.com>
2018-11-13 11:19:11 +0100

    Add toFixed to exports.

--- src/quantity/index.ts
+++ src/quantity/index.ts
@@ -3,4 +3,5 @@ export { default as createQuantity } from "./createQuantity";
 export { default as subtract } from "./subtract";
 export { default as isEqual } from "./isEqual";
 export { default as greaterThan } from "./greaterThan";
+export { default as toFixed } from "./toFixed";
 export { default as QuantityInterface } from "./QuantityInterface";
