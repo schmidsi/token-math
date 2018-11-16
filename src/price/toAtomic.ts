7cc5b52dacb2a271fd9b68fa8204b4d75e072713
Simon Emanuel Schmid <simon@schmid.io>
2018-11-16 18:38:04 +0100

    Fix import

--- src/price/toAtomic.ts
+++ src/price/toAtomic.ts
@@ -1,6 +1,6 @@
 import normalize from "./normalize";
 import PriceInterface from "./PriceInterface";
-import { BigInteger } from "bigInteger";
+import { BigInteger } from "../bigInteger";
 
 const toAtomic = (price: PriceInterface): BigInteger =>
   normalize(price).quote.quantity;
