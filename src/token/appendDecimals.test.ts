378403cb1c17ec9b1036f35793a6cfbc88ba8883
Sebastian Siemssen <fubhy@fubhy.com>
2019-01-09 12:59:47 +0100

    Fixing decimals.

--- src/token/appendDecimals.test.ts
+++ src/token/appendDecimals.test.ts
@@ -3,9 +3,11 @@ import appendDecimals from "./appendDecimals";
 test("appendDecimals", () => {
   const tknA = { symbol: "TKNA", decimals: 5 };
   const tknB = { symbol: "TKNB", decimals: 10 };
+  const tknC = { symbol: "TKNC", decimals: 18 };
 
   expect(appendDecimals(tknA, 1).toString()).toBe("100000");
   expect(appendDecimals(tknA, 1.5).toString()).toBe("150000");
   expect(appendDecimals(tknB, 2).toString()).toBe("20000000000");
   expect(appendDecimals(tknA, "2.34").toString()).toBe("234000");
+  expect(appendDecimals(tknC, "0.0022387548274030184").toString()).toBe("2238754827403018");
 });
--- src/token/appendDecimals.ts
+++ src/token/appendDecimals.ts
@@ -7,7 +7,7 @@ const appendDecimals = (
 ): BigInteger => {
   const string = typeof n === "string" ? n : n.toString();
   const int = parseInt(string).toString();
-  const decimals = string.slice(int.length + 1);
+  const decimals = string.slice(int.length + 1).slice(0, token.decimals);
   const padding = Array.apply(null, Array(Math.max(0, token.decimals - decimals.length)))
     .map(() => 0)
     .join("");
