fc4276d0294097667993ef8fe8609f48a7ea4c0b
Sebastian Siemssen <fubhy@fubhy.com>
2019-01-14 16:07:08 +0100

    Fixing bug in isPrice().

--- src/price/isPrice.ts
+++ src/price/isPrice.ts
@@ -2,6 +2,6 @@ import isQuantity from "../quantity/isQuantity";
 import PriceInterface from "./PriceInterface";
 
 const isPrice = (input: any): input is PriceInterface =>
-  isQuantity(input.base) && isQuantity(input.quote);
+  input.base && input.quote && isQuantity(input.base) && isQuantity(input.quote);
 
 export default isPrice;
