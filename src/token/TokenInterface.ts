f1fe3f24a39796c53eb0f6dc33252551d3d4a02f
Simon Emanuel Schmid <simon@schmid.io>
2019-01-16 16:19:57 +0100

    Make the TokenInteface less strict to add other fields

--- src/token/TokenInterface.ts
+++ src/token/TokenInterface.ts
@@ -2,6 +2,8 @@ interface TokenInterface {
   readonly symbol: string;
   readonly address?: string;
   readonly decimals: number;
+  readonly name?: string;
+  [key: string]: any;
 }
 
 export default TokenInterface;
